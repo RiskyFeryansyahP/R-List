ab015d3046cb9e3c2df17ac31424d196eacab563
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-08-14 01:12:21 +0700

    #1 add schema config and http handler

--- main.go
+++ main.go
@@ -2,9 +2,52 @@ package main
 
 import (
 	"fmt"
-	// "github.com/graphql-go/graphql"
+	"log"
+	"net/http"
+
+	"github.com/confus1on/R-List/database"
+	"github.com/confus1on/R-List/mutations"
+	"github.com/confus1on/R-List/queries"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
 func main() {
 	fmt.Println("R-List Building Here...!")
+	db, err := database.GetMongo()
+	if err != nil {
+		log.Fatalf("Error Can't connect into Database %s", err)
+	}
+
+	queries, _ := queries.New(db)
+	mutations, _ := mutations.New(db)
+
+	schemaConfig := graphql.SchemaConfig{
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name:   "RootQuery",
+			Fields: queries.GetRootFields(),
+		}),
+		Mutation: graphql.NewObject(graphql.ObjectConfig{
+			Name:   "RootMutation",
+			Fields: mutations.GetRootFields(),
+		}),
+	}
+
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("Failed to create new schema, error: %v", err)
+	}
+
+	httpHandler := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	http.Handle("/", httpHandler)
+	log.Print("Ready Listening....\n")
+
+	http.ListenAndServe(":8888", nil)
+
 }
