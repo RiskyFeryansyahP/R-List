3e59e8a450144df870239a5ff6f048d236cd0d91
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-08-14 01:13:34 +0700

    root queries graphql

---
+++ queries/queries.go
@@ -0,0 +1,25 @@
+package queries
+
+import (
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Queries struct {
+	Database *mongo.Database
+}
+
+func New(database *mongo.Database) (queries *Queries, err error) {
+	queries = &Queries{Database: database}
+	fmt.Println(queries)
+	return queries, nil
+}
+
+func (query *Queries) GetRootFields() graphql.Fields {
+	return graphql.Fields{
+		"user":  query.FindUserQuery(),
+		"users": query.GetAllUsers(),
+	}
+}
