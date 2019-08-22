39f8d23a565e115083d55fcaffbeb65cebc97b48
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-08-22 13:13:01 +0700

    added UserAuthentication Query

--- queries/user.go
+++ queries/user.go
@@ -2,9 +2,12 @@ package queries
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/confus1on/R-List/types"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/graphql-go/graphql"
 	"go.mongodb.org/mongo-driver/bson"
@@ -33,6 +36,44 @@ func (query *Queries) FindUserQuery() *graphql.Field {
 				return User, nil
 			}
 			return nil, nil
+		},
+	}
+}
+
+func (queries *Queries) UserAuthentication() *graphql.Field {
+	return &graphql.Field{
+		Name: "FindUser",
+		Type: types.UserType,
+		Args: graphql.FieldConfigArgument{
+			"username": &graphql.ArgumentConfig{Type: graphql.String},
+			"password": &graphql.ArgumentConfig{Type: graphql.String},
+		},
+		Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+			collection := queries.Database.Collection("user")
+			username := params.Args["username"].(string)
+			password := params.Args["password"].(string)
+
+			var User types.User
+
+			filter := bson.D{
+				primitive.E{
+					Key:   "username",
+					Value: username,
+				},
+			}
+
+			err := collection.FindOne(context.Background(), filter).Decode(&User)
+			if err != nil {
+				fmt.Printf("Error %s \n", err.Error())
+				return nil, errors.New("Can't find username")
+			}
+
+			err = bcrypt.CompareHashAndPassword([]byte(User.Password), []byte(password))
+			if err == nil {
+				return User, nil
+			}
+
+			return nil, errors.New("Password is not match")
 		},
 	}
 }
